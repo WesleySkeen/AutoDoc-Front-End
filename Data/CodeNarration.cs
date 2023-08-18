c75fa96f42feed13fbf5a8ae218167120ecff72a
Mereta <mereta.degutyte@emailage.com>
2023-08-18 10:13:23 +0100

    Adding S3 fetch

--- Data/CodeNarration.cs
+++ Data/CodeNarration.cs
@@ -1,8 +1,8 @@
 namespace AutoDoc_Front_End.Data;
 
-public class CodeNarration
+public class DocumentItem
 {
     public string FunctionName { get; set; }
-    public string Description { get; set; }
+    public string Explanation { get; set; }
     public string Code { get; set; }
 }
